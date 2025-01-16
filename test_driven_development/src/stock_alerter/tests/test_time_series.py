922475b3960a076e651e6aa081ce82e57d126116
Phuong Bui <phuong.buimai@asnet.com.vn>
2025-01-16 15:01:18 +0700

    feat(example): used docstring in test case

---
+++ test_driven_development/src/stock_alerter/tests/test_time_series.py
@@ -0,0 +1,19 @@
+import unittest
+from datetime import datetime
+
+from ..timeseries import TimeSeries
+
+
+class TimeSeriesTest(unittest.TestCase):
+    def test_closing_price_list_before_series_start_date(self):
+        """
+        Empty list is returned if on_date is before the start of the
+        series.
+        The moving average calculation might be done before any data
+        has been added to the stock. We return an empty list so that
+        the calculation can still proceed as usual.
+        """
+        series = TimeSeries()
+        series.update(datetime(2014, 2, 12), 5)
+        on_date = datetime(2014, 2, 11)
+        self.assertEqual([], series.get_closing_price_list(on_date, 1))
