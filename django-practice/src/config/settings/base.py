34426f20e3934b4ffe016e00376c60ae399c1c88
Phuong Bui <phuong.buimai@asnet.com.vn>
2025-02-24 10:13:12 +0700

    feat(config): updated SITE_ID for config admin page, created superuser

--- django-practice/src/config/settings/base.py
+++ django-practice/src/config/settings/base.py
@@ -73,6 +73,7 @@ LOCAL_APPS = [
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+SITE_ID = 1
 
 # MIGRATIONS
 # ------------------------------------------------------------------------------
