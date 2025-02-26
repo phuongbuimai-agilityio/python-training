92db63acd01105b6d23ba792cd1a996b34146b6b
Phuong Bui <phuong.buimai@asnet.com.vn>
2025-02-26 15:11:22 +0700

    feat(home-page): implemented layout template and home page for courses app

--- django-practice/src/courses/views.py
+++ django-practice/src/courses/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.views import generic
+from .models import Course
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the courses index.")
+
+
+class IndexView(generic.ListView):
+    template_name = "courses/index.html"
+    context_object_name = "latest_courses"
+
+    def get_queryset(self):
+        return Course.objects.filter(is_active=True)
