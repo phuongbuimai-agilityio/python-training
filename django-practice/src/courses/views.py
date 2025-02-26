6e9740d447908dbdcfd00dad9249021b59bf9638
Phuong Bui <phuong.buimai@asnet.com.vn>
2025-02-27 05:15:01 +0700

    feat(course): show enrolled courses for authenticated user

--- django-practice/src/courses/views.py
+++ django-practice/src/courses/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views import generic
 from .models import Course
 
@@ -11,5 +12,18 @@ class IndexView(generic.ListView):
     template_name = "courses/index.html"
     context_object_name = "latest_courses"
 
-    def get_queryset(self):
-        return Course.objects.filter(is_active=True)
+    def get_queryset(self, request):
+        active_courses = Course.objects.filter(is_active=True)
+        return render(request, "courses/index.html", {"courses": active_courses})
+
+
+def courses_list(request):
+    """Show active courses for anonymous users and enrolled courses for students"""
+    if request.user.is_authenticated:
+        student = request.user.student
+        enrolled_courses = Course.objects.filter(enrollment__student=student)
+        print(enrolled_courses)
+        return render(request, "courses/index.html", {"courses": enrolled_courses})
+    else:
+        active_courses = Course.objects.filter(is_active=True)
+        return render(request, "courses/index.html", {"courses": active_courses})
