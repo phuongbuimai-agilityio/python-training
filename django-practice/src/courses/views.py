9ea8b2d15e972d976305cebe05cbd21a1cc40adb
Phuong Bui <phuong.buimai@asnet.com.vn>
2025-03-05 05:54:28 +0700

    feat(search-filter): implement filter feature

--- django-practice/src/courses/views.py
+++ django-practice/src/courses/views.py
@@ -8,27 +8,26 @@ def courses_list(request):
     search_query = request.GET.get("q", "")
     search_filter = Q(title__icontains=search_query) if search_query else Q()
 
-    filter_option = request.GET.get("filter", "")
+    filter_option = request.GET.get("filter", "newest")
+    order_by = "-created" if filter_option == "newest" else "created"
 
     if request.user.is_authenticated:
         if hasattr(request.user, "student"):
             courses = Course.objects.filter(
                 Q(enrollment__student=request.user.student) & search_filter
-            )
+            ).order_by(order_by)
             # return render(request, "courses/index.html", {"courses": enrolled_courses})
         else:
             return render(
                 request, "courses/error.html", {"message": "User is not a student."}
             )
     else:
-        courses = Course.objects.filter(Q(is_active=True) & search_filter)
+        courses = Course.objects.filter(Q(is_active=True) & search_filter).order_by(
+            order_by
+        )
         # return render(request, "courses/index.html", {"courses": active_courses})
 
     # Sort the courses based on the filter option
-    if filter_option == "created_asc":
-        courses = courses.order_by("created")
-    elif filter_option == "created_desc":
-        courses = courses.order_by("-created")
 
     return render(
         request,
