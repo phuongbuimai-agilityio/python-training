3be052e5593b8e16fd6b0f6129f55aa8fd1ac878
Phuong Bui Mai <phuong.buimai@asnet.com.vn>
2025-03-07 15:12:20 +0700

    Delete django_example directory

--- django_example/news/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from .models import Article
-
-
-def year_archive(request, year):
-    a_list = Article.objects.filter(pub_date__year=year)
-    context = {"article_list": a_list, "year": year}
-    return render(request, "news/year_archive.html", context)
